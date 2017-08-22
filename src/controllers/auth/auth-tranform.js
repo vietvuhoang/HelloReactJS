e5c14251725c063ea30758301e3e63e6678b44ab
Vu Hoang Viet <vietvuhoang@gmail.com>
2017-08-22 15:43:16 +0700

    Refactor code

--- src/controllers/auth/auth-tranform.js
+++ src/controllers/auth/auth-tranform.js
@@ -2,18 +2,12 @@ import {createTransform } from 'redux-persist';
 import AUTH_MOD_NAME from './auth-module-name';
 import authStates from './auth-states';
 
-const authTransform = createTransform(
-    (state) => {
-        if (!state.state) return { state: authStates.AUTH_NONE };
-        else if (state.state && state.state === authStates.AUTH_PENDING) return { state: authStates.AUTH_NONE };
-        else return state;
-    },
-    (state) => {
-        if (!state.state) return { state: authStates.AUTH_NONE };
-        else if (state.state && state.state === authStates.AUTH_PENDING) return { state: authStates.AUTH_NONE };
-        else return state;
-    },
-    { whitelist: AUTH_MOD_NAME }
-);
+const ignorePendingState = (state) => {
+    if (!state.state) return { state: authStates.AUTH_NONE };
+    else if (state.state && state.state === authStates.AUTH_PENDING) return { state: authStates.AUTH_NONE };
+    else return state;
+};
+
+const authTransform = createTransform( ignorePendingState, ignorePendingState,  { whitelist: AUTH_MOD_NAME } );
 
 export default authTransform;
